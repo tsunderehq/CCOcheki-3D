cb84701641c298ce8a6e0cbd69de22e22971e779
sid410 <butaslaciii@gmail.com>
2022-12-26 18:13:28 +0900

    3 cheki anims
    adjust for each start pos, and adjust clip timing

--- Assets/Scripts/ChekiLogic.cs
+++ Assets/Scripts/ChekiLogic.cs
@@ -14,9 +14,9 @@ public class ChekiLogic : MonoBehaviour
     public GameObject ParentCanvas, CountTimerGO;
 
     //for returning manaka to original transform after animation
-    public Transform ManakaTransform;
-    private Vector3 ManakaStartPos;
-    private Quaternion ManakaStartRot;
+    public Transform Anim1, Anim2, Anim3;
+    private Vector3 Anim1Pos, Anim2Pos, Anim3Pos;
+    private Quaternion Anim1Rot, Anim2Rot, Anim3Rot;
 
     //camera flash effect
     private Flash _flash;
@@ -30,42 +30,40 @@ public class ChekiLogic : MonoBehaviour
         playerAnimator = GetComponent<Animator>();
         _flash = FindObjectOfType<Flash>();
 
-        ManakaStartPos = ManakaTransform.position;
-        ManakaStartRot = ManakaTransform.rotation;
+        InitializeAnimationTransforms();
     }
 
     private void Update()
     {
-        //press 1, 2, or 3 to trigger animation. Does not interrupt until back to Idle state
+        //only take input when in Idle mode
+        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || playerAnimator.IsInTransition(0)) return;
+
+        //press 1, 2, or 3 to trigger animation
         if (Input.GetKeyDown(KeyCode.Alpha1)) DoBigHeartAnimation();
         if (Input.GetKeyDown(KeyCode.Alpha2)) DoSmallHeartAnimation();
         if (Input.GetKeyDown(KeyCode.Alpha3)) DoNyanAnimation();
-
-        //return Manaka to initial transform after doing cheki animation
-        if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle") && !playerAnimator.IsInTransition(0))
-        {
-            if (gameObject.transform.position != ManakaStartPos && gameObject.transform.rotation != ManakaStartRot)
-            {
-                gameObject.transform.position = ManakaStartPos;
-                gameObject.transform.rotation = ManakaStartRot;
-            }
-        }
     }
 
     private void DoBigHeartAnimation()
     {
+        gameObject.transform.position = Anim1Pos;
+        gameObject.transform.rotation = Anim1Rot;
         playerAnimator.SetTrigger("BigHeart");
         StartCountDown();
     }
 
     private void DoSmallHeartAnimation()
     {
+        gameObject.transform.position = Anim2Pos;
+        gameObject.transform.rotation = Anim2Rot;
         playerAnimator.SetTrigger("SmallHeart");
         StartCountDown();
     }
 
     private void DoNyanAnimation()
     {
+        gameObject.transform.position = Anim3Pos;
+        gameObject.transform.rotation = Anim3Rot;
         playerAnimator.SetTrigger("Nyan");
         StartCountDown();
     }
@@ -74,6 +72,18 @@ public class ChekiLogic : MonoBehaviour
     {
         GameObject cdTimerInstance = Instantiate(CountTimerGO) as GameObject;
         cdTimerInstance.transform.SetParent(ParentCanvas.transform, false);
+    }
+
+    private void InitializeAnimationTransforms()
+    {
+        Anim1Pos = Anim1.position;
+        Anim1Rot = Anim1.rotation;
+
+        Anim2Pos = Anim2.position;
+        Anim2Rot = Anim2.rotation;
+
+        Anim3Pos = Anim3.position;
+        Anim3Rot = Anim3.rotation;
     }
 
     public void CountDownEnd()
