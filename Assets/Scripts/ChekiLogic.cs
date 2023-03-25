45a287328734f989629da6a367dc086be72d1833
Houssein Zaghdane <zaghdane@fireflow.de>
2023-03-26 02:21:46 +0900

    made a new animator that automatically goes through all states,still needs an idle animation and a working reset

--- Assets/Scripts/ChekiLogic.cs
+++ Assets/Scripts/ChekiLogic.cs
@@ -8,34 +8,54 @@ using TMPro;
 /// </summary>
 public class ChekiLogic : MonoBehaviour
 {
+    [Header("Camera")]
     public Camera mainCamera;
 
+    [Header("Screenshot data")]
     //for screenshot function
     [SerializeField] private ScreenshotCompanion screenshot;
     [SerializeField] private RawImage ScreenshotAnimator;
     [SerializeField] private Animator screenShotAnimator;
+    [SerializeField] private Flash _flash;
 
+    [Header("UI elements")]
     //UI gameobjects
     public GameObject ParentCanvas, CountTimePrefab;
     [SerializeField] private TextMeshProUGUI avatarNameDisplay;
 
+
+    [Header("Animations")]
     //for returning manaka to original transform after animation
     public Transform Anim1, Anim2, Anim3, Hima;
+    
 
-    //camera flash effect
-    [SerializeField] private Flash _flash;
-
+    [Header("Avatar and animators")]
     //for switching between Manaka and Kaguya
     [SerializeField] private GameObject ManakaAvatar, KaguyaAvatar;
     private GameObject CurrentAvatar;
     private Animator currentAnimator, manakaAnimator, kaguyaAnimator;
 
+    [Header("Sound effects")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip aclip_countdown;
+    [SerializeField] private AudioClip aclip_intro;
+    [SerializeField] private AudioClip aclip_outro;
 
     //private bool countdownStarted = false; //prevent double countdown
 
     private const float smallHeartStopTime = 6f;
     private const float bigHeartStopTime = 5.8f;
     private const float nyanStopTime = 6.5f;
+
+    //new : Animations are longer so no need to stop, we check the end time and start the animation at end time - 3f;
+    private const float bigHeartCountdownEndTime = 9.1f;
+    private const float bigHeartTotalAnimationTime = 14.16f;
+
+    private const float smallHeartCountdownEndTime = 9.1f;
+    private const float smallHeartTotalAnimationTime = 16.7f;
+
+    private const float nyanCountdownEndTime = 8.8f;
+    private const float nyanTotalAnimationTime = 13.23f;
 
     private GameObject cdTimerInstance;
     public enum AnimationState
@@ -53,6 +73,7 @@ public class ChekiLogic : MonoBehaviour
 
     private void Start()
     {
+        if (audioSource == null) audioSource = GetComponent<AudioSource>();
         manakaAnimator = ManakaAvatar.GetComponent<Animator>();
         kaguyaAnimator = KaguyaAvatar.GetComponent<Animator>();
 
@@ -101,7 +122,7 @@ public class ChekiLogic : MonoBehaviour
 
     public void PlayThroughAllAnimations()
     {
-        StartCoroutine(PlayThroughAllAnimationsCoroutine());
+        StartCoroutine(PlayThroughAllAnimationsCoroutineAutomatic());
     }
 
     public void GoNextAnimation()
@@ -115,7 +136,7 @@ public class ChekiLogic : MonoBehaviour
         CurrentAvatar.transform.position = Anim1.position;
         CurrentAvatar.transform.rotation = Anim1.rotation;
         currentAnimator.SetTrigger("BigHeart");
-        StartCoroutine(PauseAnimatorCoroutine(bigHeartStopTime));
+        StartCoroutine(StartCountdownAfterDelay(bigHeartCountdownEndTime - 3f));
         //StartCountDown();
         ChangeState(AnimationState.AnimatingBigHeart);
     }
@@ -125,7 +146,8 @@ public class ChekiLogic : MonoBehaviour
         CurrentAvatar.transform.position = Anim2.position;
         CurrentAvatar.transform.rotation = Anim2.rotation;
         currentAnimator.SetTrigger("SmallHeart");
-        StartCoroutine(PauseAnimatorCoroutine(smallHeartStopTime));
+        Debug.Log("triggered small heart");
+        StartCoroutine(StartCountdownAfterDelay(smallHeartCountdownEndTime - 3f));
         //StartCountDown();
         ChangeState(AnimationState.AnimatingSmallHeart);
     }
@@ -135,7 +157,7 @@ public class ChekiLogic : MonoBehaviour
         CurrentAvatar.transform.position = Anim3.position;
         CurrentAvatar.transform.rotation = Anim3.rotation;
         currentAnimator.SetTrigger("Nyan");
-        StartCoroutine(PauseAnimatorCoroutine(nyanStopTime));
+        StartCoroutine(StartCountdownAfterDelay(nyanCountdownEndTime - 3f));
         //StartCountDown();
         ChangeState(AnimationState.AnimatingNyan);
     }
@@ -146,6 +168,10 @@ public class ChekiLogic : MonoBehaviour
         cdTimerInstance = Instantiate(CountTimePrefab) as GameObject;
         cdTimerInstance.transform.SetParent(ParentCanvas.transform, false);
         cdTimerInstance.transform.localPosition = new Vector3(626f, 1286f, 0f);
+
+        //play countdown sound
+        audioSource.clip = aclip_countdown;
+        audioSource.Play();
     }
 
     public void CountDownEnd()
@@ -161,8 +187,8 @@ public class ChekiLogic : MonoBehaviour
 
         if (!_flash.gameObject.activeInHierarchy) _flash.gameObject.SetActive(true);
         _flash.CameraFlash();
-
-        currentAnimator.speed = 1; //we continue the animation
+        ChangeState(AnimationState.Finished);
+        ///currentAnimator.speed = 1; //we continue the animation
     }
 
     private IEnumerator PauseAnimatorCoroutine(float countdown)
@@ -172,21 +198,79 @@ public class ChekiLogic : MonoBehaviour
         StartCountDown();
     }
 
+    private IEnumerator StartCountdownAfterDelay(float countdown)
+    {
+        yield return new WaitForSeconds(countdown);
+        //currentAnimator.speed = 0;
+        StartCountDown();
+    }
+
     private IEnumerator PlayThroughAllAnimationsCoroutine()
     {
+        //play intro music
+        audioSource.clip = aclip_intro;
+        audioSource.Play();
+        yield return new WaitForSeconds(aclip_intro.length + 1); //wait until intro is over
+
+        //play big heart animation
         Debug.Log("doing big heart animation");
         DoBigHeartAnimation();
         while (AnimState == AnimationState.AnimatingBigHeart) yield return null;
 
+        yield return new WaitForSeconds(bigHeartTotalAnimationTime - bigHeartCountdownEndTime);
+        //play small heart animation
         Debug.Log("doing small heart animation");
         DoSmallHeartAnimation();
+        
         while (AnimState == AnimationState.AnimatingSmallHeart) yield return null;
 
+        yield return new WaitForSeconds(smallHeartTotalAnimationTime - smallHeartCountdownEndTime);
+        //play nyan animation
         Debug.Log("doing nyan animation");
         DoNyanAnimation();
         while (AnimState == AnimationState.AnimatingNyan) yield return null;
 
         Debug.Log("finished all animation");
+
+        //outro sound
+        audioSource.clip = aclip_outro;
+        audioSource.Play();
+    }
+
+
+    //reworked to automatically cycle through all animations in animator with no transition with the idle animation
+    private IEnumerator PlayThroughAllAnimationsCoroutineAutomatic()
+    {
+        //play intro music
+        currentAnimator.SetTrigger("Start");
+        audioSource.clip = aclip_intro;
+        audioSource.Play();
+        //yield return new WaitForSeconds(aclip_intro.length + 1); //wait until intro is over
+
+        while (!currentAnimator.GetCurrentAnimatorStateInfo(0).IsName("BigHeart")) yield return null;
+
+        //play big heart animation
+        Debug.Log("doing big heart animation");
+        StartCoroutine(StartCountdownAfterDelay(bigHeartCountdownEndTime - 3f));
+
+        while (!currentAnimator.GetCurrentAnimatorStateInfo(0).IsName("SmallHeart")) yield return null;
+
+        //play small heart animation
+        Debug.Log("doing small heart animation");
+        StartCoroutine(StartCountdownAfterDelay(smallHeartCountdownEndTime - 3f));
+        while (!currentAnimator.GetCurrentAnimatorStateInfo(0).IsName("Nyan")) yield return null;
+
+        //play nyan animation
+        Debug.Log("doing nyan animation");
+        
+        StartCoroutine(StartCountdownAfterDelay(nyanCountdownEndTime - 3f));
+        while (!currentAnimator.GetCurrentAnimatorStateInfo(0).IsName("Outro")) yield return null;
+
+        Debug.Log("finished all animation");
+
+        //outro sound
+        audioSource.clip = aclip_outro;
+        audioSource.Play();
     }
 
 }
